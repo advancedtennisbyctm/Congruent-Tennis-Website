8a3b43a1afdb856fe94234d1017d6484673e19c3
AdvancedTennisCTM <advancedtennisbyctm@gmail.com>
2025-03-26 22:27:35 -0400

    changed src/config

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const bcrypt = require("bcrypt");
 const session = require("express-session");
-const { connectDatabase, User } = require("./src/config");
+const { connectDatabase, User } = require("./src/config.js");
 
 const app = express();
 
