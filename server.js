292d56dc1a44e80d2573be931651970d50cc2fe9
AdvancedTennisCTM <advancedtennisbyctm@gmail.com>
2025-05-30 23:08:22 -0400

    fix sorting error

--- server.js
+++ server.js
@@ -392,9 +392,11 @@ const upload = multer({ storage: multer.memoryStorage() });
 
 app.get("/articles", async (req, res) => {
   try {
-    const articles = await Article.find({}, { article_data: 0, image_data: 0 }) // exclude large fields
-      .sort({ date: -1 })
-      .limit(100); // adjust or paginate
+        const articles = await Article.aggregate([
+        { $project: { article_data: 0, image_data: 0 } },
+        { $sort: { date: -1 } },
+        { $limit: 100 }
+        ], { allowDiskUse: true });
 
     res.json(articles);
   } catch (err) {
